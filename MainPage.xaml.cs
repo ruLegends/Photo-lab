cd372189eb3a60313668161dc13ab26d1308ed17
Dinislam <ruLegens@gmail.com>
2020-11-29 15:06:16 +0300

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,165 @@
+﻿using Album.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace Album
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public static MainPage Current;
+
+        public double ItemSize
+        {
+            get => _itemSize;
+            set
+            {
+                if (_itemSize != value)
+                {
+                    _itemSize = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ItemSize)));
+                }
+            }
+        }
+        private double _itemSize;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Current = this;
+            GetItemsAsync();
+        }
+        private async Task GetItemsAsync()
+        {
+            //StorageFile Images = new StorageFile();
+            List<ImageFileInfo> Images = new List<ImageFileInfo>();
+
+            QueryOptions options = new QueryOptions();
+            options.FolderDepth = FolderDepth.Deep;
+            options.FileTypeFilter.Add(".jpg");
+            options.FileTypeFilter.Add(".png");
+            options.FileTypeFilter.Add(".gif");
+            options.FileTypeFilter.Add(".jpeg");
+
+            StorageFolder appInstalledFolder = Package.Current.InstalledLocation;
+            StorageFolder picturesFolder = await
+
+            appInstalledFolder.GetFolderAsync("Assets\\Samples");
+            var result = picturesFolder.CreateFileQueryWithOptions(options);
+
+            IReadOnlyList<StorageFile> imageFiles = await result.GetFilesAsync();
+            bool unsupportedFilesFound = false;
+
+            foreach (StorageFile file in imageFiles)
+            {             
+                if (file.Provider.Id == "computer")
+                {
+                    Images.Add(await LoadImageInfo(file));
+                    Console.WriteLine("Download " + file.Name);
+                }
+                else
+                {
+                    unsupportedFilesFound = true;
+                }
+            }
+
+            ImageGridView.ItemsSource = Images;
+
+            if (unsupportedFilesFound == true)
+            {
+                ContentDialog unsupportedFilesDialog = new ContentDialog
+                {
+                    Title = "Unsupported images found",
+                    Content = "This sample app only supports images stored locally on the computer." +
+                    "We found files in your library that are stored in OneDrive or anothernetwork location.We didn't load those images.",
+                    CloseButtonText = "Ok"
+                };
+                ContentDialogResult resultNotUsed = await unsupportedFilesDialog.ShowAsync();
+            }
+        }
+        public async static Task<ImageFileInfo> LoadImageInfo(StorageFile file)
+        {
+            var properties = await file.Properties.GetImagePropertiesAsync();
+            ImageFileInfo info = new ImageFileInfo(properties, file, file.DisplayName, file.DisplayType);
+            return info;
+        }
+        private void ImageGridView_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.InRecycleQueue)
+            {
+                var templateRoot = args.ItemContainer.ContentTemplateRoot as Grid;
+                var image = (Image)templateRoot.FindName("ItemImage");
+                image.Source = null;
+            }
+            if (args.Phase == 0)
+            {
+                args.RegisterUpdateCallback(ShowImage);
+                args.Handled = true;
+            }
+        }
+        private async void ShowImage(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.Phase == 1)
+            {
+                var templateRoot = args.ItemContainer.ContentTemplateRoot as Grid;
+                var image = (Image)templateRoot.FindName("ItemImage");
+                image.Opacity = 100;
+                var item = args.Item as ImageFileInfo;
+                try
+                {
+                    image.Source = await item.GetImageThumbnailAsync();
+                }
+                catch (Exception)
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.UriSource = new Uri(image.BaseUri, "Assets/StoreLogo.png");
+                    image.Source = bitmapImage;
+                }
+            }
+        }
+        private void DetermineItemSize()
+        {
+            if (
+            FitScreenToggle != null
+            && FitScreenToggle.IsOn == true
+            && ImageGridView != null
+            && ZoomSlider != null)
+            {
+                int margins = (int)this.Resources["LargeItemMarginValue"] * 4;
+                double gridWidth = ImageGridView.ActualWidth - (int)this.Resources["DefaultWindowSidePaddingValue"];
+                double ItemWidth = ZoomSlider.Value + margins;
+                int columns = (int)Math.Max(gridWidth / ItemWidth, 1);
+                double adjustedGridWidth = gridWidth - (columns * margins);
+                ItemSize = (adjustedGridWidth / columns);
+            }
+            else
+            {
+                ItemSize = ZoomSlider.Value;
+            }
+        }
+
+    }
+}
---
+++ ViewModel/ImageFileInfo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Album.ViewModel
+{
+    public class ImageFileInfo
+    {
+        public ImageFileInfo(ImageProperties properties, Windows.Storage.StorageFile imageFile, string name, string type)
+        {
+            ImageProperties = properties;
+            ImageName = name;
+            ImageFileType = type;
+            ImageFile = imageFile;
+            var rating = (int)properties.Rating;
+            var random = new Random();
+            ImageRating = rating == 0 ? random.Next(1, 5) : rating;
+        }
+        public StorageFile ImageFile { get; }
+        public ImageProperties ImageProperties { get; }
+        public string ImageName { get; }
+        public string ImageFileType { get; }
+        public string ImageDimensions =>
+        $"{ImageProperties.Width} x {ImageProperties.Height}";
+        public string ImageTitle
+        {
+            get => String.IsNullOrEmpty(ImageProperties.Title) ? ImageName :
+            ImageProperties.Title;
+            set
+            {
+                if (ImageProperties.Title != value)
+                {
+                    ImageProperties.Title = value;
+                    var ignoreResult = ImageProperties.SavePropertiesAsync();
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int ImageRating
+        {
+            get => (int)ImageProperties.Rating;
+            set
+            {
+                if (ImageProperties.Rating != value)
+                {
+                    ImageProperties.Rating = (uint)value;
+                    var ignoreResult = ImageProperties.SavePropertiesAsync();
+                    OnPropertyChanged();
+                }
+            }
+
+        }
+        public async Task<BitmapImage> GetImageThumbnailAsync()
+        {
+            var thumbnail = await ImageFile.GetThumbnailAsync(ThumbnailMode.PicturesView);
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.SetSource(thumbnail);
+            thumbnail.Dispose();
+            return bitmapImage;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+}
